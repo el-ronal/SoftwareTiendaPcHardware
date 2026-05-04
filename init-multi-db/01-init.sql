59d1d934762be66c840e20afc8b594f5170e6fe1
AO-Alumno <AO-Alumno@duoc.acad>
2026-05-04 13:10:41 -0400

    Arreglo de proyecto

--- init-multi-db/01-init.sql
+++ init-multi-db/01-init.sql
@@ -11,31 +11,31 @@ SELECT 'CREATE DATABASE stock'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stock') \gexec
 
 SELECT 'CREATE DATABASE pedidos'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stock') \gexec
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'pedidos') \gexec
 
 SELECT 'CREATE DATABASE pagos'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stock') \gexec
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'pagos') \gexec
 
 SELECT 'CREATE DATABASE envios'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stock') \gexec
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'envios') \gexec
 
 SELECT 'CREATE DATABASE tasacion'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stock') \gexec
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'tasacion') \gexec
 
 SELECT 'CREATE DATABASE garantias'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stock') \gexec
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'garantias') \gexec
 
 SELECT 'CREATE DATABASE devoluciones'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stock') \gexec
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'devoluciones') \gexec
 
 SELECT 'CREATE DATABASE soporte'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stock') \gexec
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'soporte') \gexec
 
 SELECT 'CREATE DATABASE armado'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stock') \gexec
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'armado') \gexec
 
 SELECT 'CREATE DATABASE notificaciones'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stock') \gexec
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'notificaciones') \gexec
 
 /* 
 stock
